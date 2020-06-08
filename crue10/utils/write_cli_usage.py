9a684307c3abd6b8715f39cc561ff11318952115
Luc Duron <duron.luke@gmail.com>
2020-06-08 18:21:23 +0200

    Ajout LoiTQruis et modif write_cli_usage

--- crue10/utils/write_cli_usage.py
+++ crue10/utils/write_cli_usage.py
@@ -30,19 +30,20 @@ class CommandLineScript:
         return getattr(mod, 'parser').format_help().replace(os.path.basename(__file__), os.path.basename(self.path))
 
 
-# Build sorted list of CLI scripts
-with open(os.path.join(FOLDER_DOC, '_Sidebar.md'), 'w') as out_sidebar:
-    for file_path in sorted(glob(os.path.join(FOLDER_SCRIPTS, '*.py'))):
-        script_name = os.path.splitext(os.path.basename(file_path))[0]
+if __name__ == "__main__":
+    # Build sorted list of CLI scripts
+    with open(os.path.join(FOLDER_DOC, '_Sidebar.md'), 'w') as out_sidebar:
+        for file_path in sorted(glob(os.path.join(FOLDER_SCRIPTS, '*.py'))):
+            script_name = os.path.splitext(os.path.basename(file_path))[0]
 
-        if not script_name.startswith('_'):
-            out_sidebar.write('* [[%s]]\n' % script_name)
+            if not script_name.startswith('_'):
+                out_sidebar.write('* [[%s]]\n' % script_name)
 
-            script = CommandLineScript(file_path)
+                script = CommandLineScript(file_path)
 
-            # Write a markdown file (to be integrated within github wiki)
-            with open(os.path.join(FOLDER_DOC, script_name + '.md'), 'w') as fileout:
-                fileout.write('```\n')
-                fileout.write(script.help_msg())
-                fileout.write('```\n')
-                fileout.write('\n')
+                # Write a markdown file (to be integrated within github wiki)
+                with open(os.path.join(FOLDER_DOC, script_name + '.md'), 'w') as fileout:
+                    fileout.write('```\n')
+                    fileout.write(script.help_msg())
+                    fileout.write('```\n')
+                    fileout.write('\n')
