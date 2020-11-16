eeef435c2a761a8a4f9ee80d0535ab29a666a6a3
Luc DURON <L.DURON@cnr.tm.fr>
2020-11-16 16:32:29 +0100

    Ajout make_sphinx_docs.bat

---
+++ make_sphinx_docs.bat
@@ -0,0 +1,10 @@
+@echo off
+
+REM Build docs folder with conf.py
+REM sphinx-quickstart
+
+REM Update docs/crue10.*rst
+sphinx-apidoc --ext-autodoc -d 4 -o docs crue10
+
+REM Build HTML documentation locally
+sphinx-build -b html docs docs\html
