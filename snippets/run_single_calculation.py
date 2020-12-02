bbe572e6fa123316f35d2933e7dd63a6601ffac6
Luc DURON <L.DURON@cnr.tm.fr>
2020-12-02 10:16:34 +0100

    MAJ run_single_calculation en séquençant le lancement des services

--- snippets/run_single_calculation.py
+++ snippets/run_single_calculation.py
@@ -12,7 +12,14 @@ etude.read_all()
 
 scenario = etude.get_scenario_courant()
 
+# Méthode 1 : tous les services à la fois
 run = scenario.create_and_launch_new_run(etude, run_id="test", force=True)
-print(run.get_all_traces())
+print(run.get_all_traces_above_warn())
+
+# Méthode 2 : en séquançant le lancement des services
+run = scenario.create_new_run(etude, run_id="test", force=True)
+run.launch_services(['r', 'g', 'i'])
+run.launch_services(['c'])
+print(run.get_all_traces_above_warn())
 
 etude.write_etu()
