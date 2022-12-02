e08c23f9982effd168b595f42eebcfbaaa74d880
Nabil BEN-SALEM <n.ben-salem@cnr.tm.fr>
2022-12-02 09:55:14 +0100

    Ajout version grammaire et passage arguments en obligatoires pour crue10_merge_submodels.py

--- cli/crue10_merge_submodels.py
+++ cli/crue10_merge_submodels.py
@@ -14,28 +14,34 @@ from crue10.utils.cli_parser import MyArgParse
 
 def crue10_merge_sous_modeles(args):
     if len(args.etu_path_list) != len(args.sm_name_list) != len(args.suffix_list):
-        raise ExceptionCrue10("Les arguments `etu_path_list`, `suffix_list` et `sm_name_list` n'ont pas la même longueur !")
+        raise ExceptionCrue10("Les arguments `etu_path_list`, `suffix_list` "
+                              "et `sm_name_list` n'ont pas la même longueur !")
+    if len(args.etu_path_list) < 2:
+        raise ExceptionCrue10("Il faut au moins 2 sous-modèles pour faire la fusion !")
 
     merged_sous_modele = None
     for etu_path, sm_name, suffix in zip(args.etu_path_list, args.sm_name_list, args.suffix_list):
         etude = Etude(etu_path)
         sous_modele = etude.get_sous_modele(sm_name)
         sous_modele.read_all()
-        print(sous_modele)
+        logger.info(sous_modele)
 
         if merged_sous_modele is None:
-            merged_sous_modele = SousModele(sous_modele.id, files=sous_modele.files)
+            merged_sous_modele = SousModele(sous_modele.id, files=sous_modele.files,
+                                            version_grammaire=sous_modele.version_grammaire)
         merged_sous_modele.ajouter_emh_depuis_sous_modele(sous_modele, suffix)
 
-    print("~> Sous-modèle fusionné:")
-    print(merged_sous_modele)
+    logger.info("~> Sous-modèle fusionné:")
+    logger.info(merged_sous_modele)
     merged_sous_modele.write_all(args.output_folder, folder_config='Config')
 
 
 parser = MyArgParse(description=__doc__)
-parser.add_argument('--etu_path_list', help="liste des chemins vers des fichiers etu.xml", nargs='+')
-parser.add_argument('--sm_name_list', help="liste des noms des sous-modèles", nargs='+')
-parser.add_argument('--suffix_list', help="liste des sufixes", nargs='+')
+parser_submodels = parser.add_argument_group("Choix des sous-modèles à fusionner")
+parser_submodels.add_argument('--etu_path_list', help="liste des chemins vers des fichiers etu.xml", nargs='+',
+                              required=True)
+parser_submodels.add_argument('--sm_name_list', help="liste des noms des sous-modèles", nargs='+', required=True)
+parser_submodels.add_argument('--suffix_list', help="liste des suffixes", nargs='+', required=True)
 parser.add_argument('output_folder', help="nom du dossier de sortie")
 
 
