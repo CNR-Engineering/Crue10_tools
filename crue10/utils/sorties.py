46c3664b36e820774776741b316b1804ca09c5a3
Luc DURON <l.duron@cnr.tm.fr>
2023-01-24 15:10:36 +0100

    Prise en compte sorties pour écriture OCAL

--- crue10/utils/sorties.py
+++ crue10/utils/sorties.py
@@ -1,8 +1,11 @@
 # coding: utf-8
 from crue10.utils import PREFIX
+from crue10.utils import ExceptionCrue10
 
 
 BOOLEAN_TAGS = ['SortieEcran', 'SortieFichier']
+SEVERITE_TAGS = ['VerbositeEcran', 'VerbositeFichier']
+TYPE_SEVERITE = ['DEBUG3', 'DEBUG2', 'DEBUG1', 'FCT3', 'FCT2', 'FCT1', 'INFO', 'WARN', 'ERRNBLK', 'ERRBLK', 'FATAL']
 
 
 def elt_to_dict(elt):
@@ -12,13 +15,23 @@ def elt_to_dict(elt):
         if tag_name in BOOLEAN_TAGS:
             res[tag_name] = sub_elt.text == 'true'
         else:
+            if tag_name in SEVERITE_TAGS:
+                if sub_elt.text not in TYPE_SEVERITE:
+                    raise ExceptionCrue10("La verbosité `%s` n'est pas standard" % sub_elt.text)
             res[tag_name] = sub_elt.text
     return res
 
 
 class Sorties:
     """
-    Sortie: paramétrage des traces et fichiers de sortie
+    Sortie: paramétrage des traces et fichier de sortie des pré-traitements et du calcul
+
+    :ivar avancement: avancement
+    :vartype avancement: dict
+    :ivar trace: trace
+    :vartype trace: dict
+    :ivar resultat: résultat
+    :vartype resultat: dict
     """
 
     def __init__(self):
@@ -43,3 +56,6 @@ class Sorties:
 
     def set_resultat(self, elt):
         self.resultat = elt_to_dict(elt)
+
+    def __repr__(self):
+        return "Sortie: avancement ({}), trace ({}), résultat ({})".format(self.avancement, self.trace, self.resultat)
