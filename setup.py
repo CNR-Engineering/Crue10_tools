fc0107dab9137e2dbcb5a621611278b7af6e6f3d
Luc DURON <l.duron@cnr.tm.fr>
2024-05-24 16:11:43 +0200

    Ajout package_data

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 from setuptools import find_packages, setup
+
 from crue10 import VERSION
 
 
@@ -13,6 +14,7 @@ setup(
     author='Luc DURON',
     author_email='l.duron@cnr.tm.fr',
     packages=find_packages(),
+    package_data={'crue10': ['crue10/data/*']},
     install_requires=requirements,
     description='Librairie Python pour les formats de fichiers Crue10',
     url='https://github.com/CNR-Engineering/crue10_tools',
